8f49d263845ae55b15b7668cf1e7c599cf33dd7d
Muhammad Syarif Hayat <muhammadsyarifhayat@gmail.com>
2026-02-28 13:35:01 +0700

    feat: create product detail page with hero, content, CTA, and footer sections.

--- app/(landing)/product/[slug]/page.tsx
+++ app/(landing)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import ProductDetailHero from "@/components/modules/landing/product/product-detail-hero"
-import ProductDetailContent from "@/components/modules/landing/product/product-detail-content"
 import CtaSection from "@/components/modules/landing/home/cta-section"
 import Footer from "@/components/modules/landing/home/footer"
+import ProductDetailContent from "@/components/modules/landing/product/product-detail-content"
+import ProductDetailHero from "@/components/modules/landing/product/product-detail-hero"
 
 const ProductDetailPage = () => {
     return (
